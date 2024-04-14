d3951ee5c710b38238df1aaca2c9edf5bfc92aa4
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2024-04-14 15:39:03 -0400

    Update RetrievalEngine.py

--- pdfchat/RetrievalEngine/RetrievalEngine.py
+++ pdfchat/RetrievalEngine/RetrievalEngine.py
@@ -12,9 +12,13 @@ class RetrievalEngine:
     ):
         assert vector_store.namespace != None, "Namespace must be set in the vector store"
         self.vector_store = vector_store
+        self.index = VectorStoreIndex.from_vector_store(self.vector_store)
         self.retriever = VectorStoreIndex.from_vector_store(self.vector_store).as_retriever(
             similarity_top_k=top_k
         )
 
     def as_retriever(self):
         return self.retriever
+    
+    def as_index(self):
+        return self.index
